e23175b74b95aed140aa769044916daed2f25ed3
aasur <x3imal@bk.ru>
2026-01-26 22:28:25 +0300

    add-favorite-page: реализовал добавление треков на страницу "мои треки" через кнопку "лайка" и наоборот

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,25 @@
 import Link from "next/link";
+import styles from "./not-found.module.css";
 
 export default function NotFound() {
     return (
-        <Link href={'/music/main'}>На главную</Link>
-    )
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <div className={styles.card}>
+                    <div className={styles.code}>404</div>
+                    <div className={styles.title}>
+                        Страница не найдена 😭
+                    </div>
+                    <div className={styles.subtitle}>
+                        Возможно, она была удалена<br />
+                        или перенесена на другой адрес
+                    </div>
+
+                    <Link href="/music/main" className={styles.button}>
+                        Вернуться на главную
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
